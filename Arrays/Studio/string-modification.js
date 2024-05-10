289cdff49b5aba4d0cda9fa62b5865d24eae97bb
Dhana bayya <krishnabayya@Dhanas-mac-Air.lan>
2024-05-09 21:54:31 -0500

    Part1

---
+++ Arrays/Studio/string-modification.js
@@ -0,0 +1,23 @@
+const input = require('readline-sync');
+let str = "LaunchCode";
+let question =input.question("pls enter the number of letters that will be relocated.");
+//a) Use string methods to remove the first three characters from the string and add them to the end.
+//Hint - define another variable to hold the new string or reassign the new string to str.
+
+if(str.length<question){
+    question=3;
+   
+    console.log("Error: The given number is undefined index");
+    console.log(`The number you given is larger than length of ${str} so we do default no 3`);
+}
+let str1 = str.slice(0,question);
+let str2 = str.slice(question,str.length);
+let str3 = str2.concat(str1);
+console.log(str3);
+//Use a template literal to print the original and modified string in a descriptive phrase.
+console.log(`${str} is the original string from this we are getting modified string of ${str3}`);
+
+
+//b) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+
+//c) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
